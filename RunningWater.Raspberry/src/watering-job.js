a7eed613a8cdded82cef0c15372003645c473564
Виктор Кочетков <victor.development@yandex.ru>
2022-04-10 23:55:53 +0300

    watering job configuration

--- RunningWater.Raspberry/src/watering-job.js
+++ RunningWater.Raspberry/src/watering-job.js
@@ -1,10 +1,26 @@
 const { exec } = require('child_process');
 
+// Command line arguments
+const args = require('minimist')(process.argv.slice(2))
+
+// Latency for USB ports power on/off
+var latency = parseInt(args['latency']) || 500;
+
+// USB ports power duration
+var duration = parseInt(args['duration']) || 3000;
+
+// Actual watering duration
+var actualDuration = Math.max(0, duration - latency);
+
+console.log(`Latency = ${latency}`);
+console.log(`Duration = ${duration}`);
+console.log(`Actual duration = ${actualDuration}`);
+
 new Promise(function(resolve) {
   exec('sudo /home/pi/uhubctl/uhubctl -a on -l 1-1', () => resolve());
 })
 .then(function() {
-  return new Promise((resolve) => setTimeout(() => resolve(), 3*1000));
+  return new Promise((resolve) => setTimeout(() => resolve(), actualDuration));
 })
 .then(function() {
   return new Promise((resolve) => exec('sudo /home/pi/uhubctl/uhubctl -a off -l 1-1', () => resolve()));
